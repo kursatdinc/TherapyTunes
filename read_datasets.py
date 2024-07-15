cd4d624800ab671b3a380805428f471cf4d4553e
kursatdinc <kursat@kursatdinc.io>
2024-07-15 12:52:40 +0300

    change datasets

--- read_datasets.py
+++ read_datasets.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 pd.set_option('display.max_columns', None)
 
@@ -20,7 +21,7 @@ songs_df.head(10)
 ###########
 songs_df = songs_df.rename(columns={"name": "song"})
 merged_df = pd.merge(lyrics_df, songs_df, how="inner", on="song")
-merged_df.shape # (21015, 24)
+merged_df.shape #(21015, 24)
 merged_df.to_csv("./datasets/song_lyric.csv", index=False)
 
 song_lyric_df = pd.read_csv("./datasets/song_lyric.csv")
@@ -34,4 +35,19 @@ combined_features_df.drop(columns=["Unnamed: 0", "...1",
 combined_features_df.dropna(inplace=True)
 
 combined_features_df.to_csv("./datasets/combined_features_disorder.csv", index=False)
+###########
+
+###########
+combined_df = pd.read_csv("./datasets/combined_features_disorder.csv")
+combined_df.shape #(526680, 12)
+
+def remove_random_percentage(dataframe, percentage):
+    num_rows_to_remove = int(len(dataframe) * percentage)
+    rows_to_remove = np.random.choice(dataframe.index, size=num_rows_to_remove, replace=False)
+    dataframe_dropped = dataframe.drop(rows_to_remove)
+    return dataframe_dropped
+
+combined_df = remove_random_percentage(combined_df, 0.3)
+
+combined_df.to_csv("./datasets/combined_features_disorder.csv", index=False)
 ###########
