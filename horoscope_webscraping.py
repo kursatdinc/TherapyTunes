cfd33aa6b016444e485778813332ac7c0d55e5db
kursatdinc <kursat@kursatdinc.io>
2024-07-17 11:32:44 +0300

    modified:   horoscope_webscraping.py

--- horoscope_webscraping.py
+++ horoscope_webscraping.py
@@ -10,22 +10,27 @@ def get_star_ratings(sign, date='today'):
     
     response = requests.get(url)
     
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, "html.parser")
     
-    rating_tags = soup.find_all('h3')
-    
-    desired_categories = ['sex', 'hustle', 'vibe', 'success']
+    rating_tags = soup.find_all("h3")
     
     ratings = {}
     for tag in rating_tags:
-        category = tag.text.split()[0].lower()  # İlk kelimeyi al (Sex, Hustle, Vibe, Success)
-        if category in desired_categories:
-            highlighted_stars = tag.find_all('i', class_='icon-star-filled highlight')
+        category = tag.text.split()[0].lower()
+        if category in ["sex", "hustle", "vibe", "success"]:
+            highlighted_stars = tag.find_all("i", class_="icon-star-filled highlight")
             stars = len(highlighted_stars)
-            ratings[category] = f"{stars}/5"
+            # ratings[category] = f"{stars}/5"
+            ratings[category] = f"{stars}"
     
     return ratings
 
+rating  = get_star_ratings("gemini")
+
+int(rating.get("sex"))
+int(rating.get("hustle"))
+int(rating.get("vibe"))
+int(rating.get("success"))
 
 for sign in sign_list:
     print(sign, get_star_ratings(sign))
