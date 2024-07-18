de348f3036d829320c60c9ec803fc03544c6c6a0
kursatdinc <kursat@kursatdinc.io>
2024-07-18 22:45:16 +0300

    modify streamlit

--- streamlit.py
+++ streamlit.py
@@ -2,21 +2,33 @@ import streamlit as st
 from st_clickable_images import clickable_images
 
 questions = [
-    {
-        "question": "TARAFINI SEÇ",
-        "choices": ["Paris", "London", "Berlin", "Rome"],
-        "image_urls": ["https://images.unsplash.com/photo-1499856871958-5b9627545d1a?w=700",
-                       "https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?w=700",
-                       "https://images.unsplash.com/photo-1560969184-10fe8719e047?w=700",
-                       "https://images.unsplash.com/photo-1552832230-c0197dd311b5?w=700"]
+        {
+        "type": "slider",
+        "question": "Enter your age.",
+        "min_value": 1,
+        "max_value": 100
     },
     {
-        "question": "Which planet is known as the Red Planet?",
-        "choices": ["Earth", "Mars", "Jupiter", "Saturn"],
-        "image_urls": ["https://images.unsplash.com/photo-1614730321146-b6fa6a46bcb4?w=700",
-                       "https://images.unsplash.com/photo-1614728263952-84ea256f9679?w=700",
-                       "https://images.unsplash.com/photo-1630839437035-dac17da580d0?w=700",
-                       "https://images.unsplash.com/photo-1614314107768-6018061b5b72?w=700"]
+        "type": "image",
+        "question": "Select the music platform that you use?",
+        "choices": ["Spotify", "YouTube Music", "Apple Music", "Other"],
+        "image_urls": ["https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Spotify_logo_with_text.svg/1200px-Spotify_logo_with_text.svg.png",
+                       "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/YT_Music.svg/1024px-YT_Music.svg.png",
+                       "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Apple_Music_logo.svg/2560px-Apple_Music_logo.svg.png",
+                       "https://www.edagroups.com/files/3214/7969/6498/other.png"]
+    },
+    {
+        "type": "slider",
+        "question": "How many hours a day do you listen to music?",
+        "min_value": 1,
+        "max_value": 24
+    },
+    {
+        "type": "image",
+        "question": "Do you listen to music while working?",
+        "choices": ["Yes", "No"],
+        "image_urls": ["https://icon2.cleanpng.com/20180319/opq/kisspng-computer-icons-clip-art-check-yes-ok-icon-5ab061dfcd38e3.7297168415215088318406.jpg",
+                       "https://banner2.cleanpng.com/20180424/wzw/kisspng-no-symbol-sign-clip-art-prohibited-signs-5adf452cc063a7.6278734415245816767881.jpg"]
     },
 ]
 
@@ -34,36 +46,37 @@ def initialize_session_state():
     if "user_answers" not in st.session_state:
         st.session_state.user_answers = []
 
-
-st.title("Quiz with Clickable Images")
+st.title("Quiz with Clickable Images and Sliders")
 
 initialize_session_state()
 
 quiz_data = st.session_state.quiz_data
 
 if quiz_data:
-    st.markdown(f"**Question {st.session_state.question_index + 1}: {quiz_data["question"]}**")
+    st.markdown(f"**Question {st.session_state.question_index + 1}: {quiz_data['question']}**")
 
-    clicked = clickable_images(
-        quiz_data["image_urls"],
-        titles=[choice for choice in quiz_data["choices"]],
-        div_style={"display": "flex", "justify-content": "center", "flex-wrap": "wrap"},
-        img_style={"margin": "5px", "height": "200px"},
-    )
+    if quiz_data["type"] == "image":
+        clicked = clickable_images(
+            quiz_data["image_urls"],
+            titles=[choice for choice in quiz_data["choices"]],
+            div_style={"display": "flex", "justify-content": "center", "flex-wrap": "wrap"},
+            img_style={"margin": "5px", "height": "200px"},
+        )
 
-    if clicked > -1:
-        selected_answer = quiz_data["choices"][clicked]
-        st.session_state.user_answers.append(selected_answer)
+        if clicked > -1:
+            selected_answer = quiz_data["choices"][clicked]
+            st.session_state.user_answers.append(selected_answer)
+            st.session_state.question_index += 1
+            st.session_state.quiz_data = get_question(st.session_state.question_index)
+            st.rerun()
 
-        # Sonraki soruya geç
-        st.session_state.question_index += 1
-        st.session_state.quiz_data = get_question(st.session_state.question_index)
+    elif quiz_data["type"] == "slider":
+        slider_value = st.slider("Your answer", min_value=quiz_data["min_value"], max_value=quiz_data["max_value"])
         
-        if st.session_state.quiz_data is None:
-            st.markdown("Quiz tamamlandı. İşte cevaplarınız:")
-            for i, answer in enumerate(st.session_state.user_answers):
-                st.write(f"Soru {i+1}: {answer}")
-        else:
+        if st.button("Submit"):
+            st.session_state.user_answers.append(slider_value)
+            st.session_state.question_index += 1
+            st.session_state.quiz_data = get_question(st.session_state.question_index)
             st.rerun()
 
 else:
